cf4f73820a6a23ffadd12f1c59fc58fa462fa440
KouameOura <kouameoura@gmail.com>
2023-02-09 23:07:15 -0500

    Back end s3 bucket created to store tfstate file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3" {
+    bucket = "terraform-demo2023"
+    key    = "terraform-demo2023/dev"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
     region = var.region
 }
